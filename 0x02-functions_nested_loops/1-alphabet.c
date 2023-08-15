4b0ac7faf32516114a7f4e0f0e8bbfa405dd451c
TheElvace <anselmelvis62@gmail.com>
2023-08-15 11:57:30 +0100

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}

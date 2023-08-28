1540404d04dfb0a4f68844622dc5f57284a45eaf
TheElvace <anselmelvis62@gmail.com>
2023-08-28 18:28:55 +0100

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * _memcpy - copies @n bytes from memory area @src
+ * _memcpy - function copies @n bytes from memory area @src
  * to memory area @dest
  *
- * @dest: Pointer to the destination array where the content is to be copied.
- * @src: Pointer to the source of data to be copied.
- * @n: Number of bytes to copy.
- * @return A pointer to the destination array `dest`.
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,6 @@
  * @s - string charcter is loacted in
  * Return: pointer to the first occurrence of the character
  */
-
 char *_strchr(char *s, char c)
 {
 	while (*s)

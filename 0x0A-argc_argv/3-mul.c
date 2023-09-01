dd0025253cb7229abbfbb61ea5c5b1564acf32a0
TheElvace <anselmelvis62@gmail.com>
2023-09-01 17:27:14 +0100

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,47 +8,48 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, len, m, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
-	j = 0;
-	k = 0;
+	d = 0;
+	n = 0;
 	len = 0;
-	m = 0;
+	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (i < len && m == 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++j;
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (j % 2)
+			if (d % 2)
 				digit = -digit;
-			k = k * 10 + digit;
-			m = 1;
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			m = 0;
+			f = 0;
 		}
 		i++;
 	}
 
-	if (m == 0)
+	if (f == 0)
 		return (0);
 
 	return (n);
 }
 
 /**
- * main - this multiplies two numbers
- * @argc: the number of arguments
+ * main - multiplies two numbers
+ * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
